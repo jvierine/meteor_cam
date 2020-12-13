ad83204169b87fe6bfa89018439a78c8c7ad0640
Juha Vierinen <jvierine@gmail.com>
2020-12-13 23:54:14 +0000

    asd

--- diff_det.py
+++ diff_det.py
@@ -2,47 +2,160 @@ import cv2
 import numpy as n
 import matplotlib.pyplot as plt
 import glob
+import sys
+import imageio
+from skimage.measure import compare_ssim
+from skimage.measure import block_reduce
+import os
 
-def diff_img_stack(fname="full_44.mp4",use_diff=False,diff_img=None):
-    cap = cv2.VideoCapture('full_44.mp4')
-    ret,frame0 = cap.read()
+#from skimage.measure import compare_ssim as ssim
+from mpi4py import MPI
+comm = MPI.COMM_WORLD    
 
-    if not use_diff:
-        diff_img=n.zeros([frame0.shape[0],frame0.shape[1]],dtype=n.float32)
-        
-    frame_bw=n.zeros([frame0.shape[0],frame0.shape[1]],dtype=n.float32)
+
+def img_var_est(fname="full_59.mp4",
+                cam_0=1490,
+                cam_1=1536,
+                dec=4,
+                plot=True,
+                median_len=30):
     
-    prev=None
-    first=True
-    frame_num=0
+    cap = cv2.VideoCapture(fname)
+    ret,frame0 = cap.read()
+    frame0[cam_0:cam_1,:,:]=0
+    prev=n.array(block_reduce(cv2.cvtColor(frame0,cv2.COLOR_BGR2GRAY),block_size=(dec,dec),func=n.max),dtype=n.float32)
+#    plt.imshow(prev)
+ #   plt.show()
+    w=prev.shape[0]
+    h=prev.shape[1]
+    stdest=n.zeros([w,h],dtype=n.float32)
+    H=n.zeros([median_len,w,h],dtype=n.float32)    
+    n_avg=0.0
+    idx=0
     while(1):
         ret,frame0 = cap.read()
-        frame0=n.array(frame0,dtype=n.float32)
         if not ret:
             break
-        frame_num+=1
-        print("frame %d"%(frame_num))
-        
-        frame_bw=frame0[:,:,0]+frame0[:,:,1]+frame0[:,:,2]
-        if not first:
-            diff_img = n.maximum( frame_bw-prev,diff_img)
-        else:
-            first=False
-
-        prev=frame_bw
-    video_capture.release()
-        
-    return(diff_img)
-
-if __name__ == "__main__":
-    fl=glob.glob("%s/*.mp4"%(sys.argv[1]))
-    
-    diff_img=diff_img_stack(fname=fl[0],use_diff=False)
-    for f in fl[1:len(fl)]:
-        diff_img=diff_img_stack(fname=f,use_diff=True,diff_img=diff_img)
-        plt.imshow(diff_img,cmap="gray")
+        frame0[cam_0:cam_1,:,:]=0                    
+        gray = n.array(block_reduce(cv2.cvtColor(frame0,cv2.COLOR_BGR2GRAY),block_size=(dec,dec),func=n.max),dtype=n.float32)
+        prev=gray
+        H[idx,:,:]=gray
+        idx+=1
+#        print(idx)
+        if idx == median_len:
+            stdest+=n.var(H,axis=0)+1.0
+            n_avg+=1.0
+            idx=0
+    cap.release()
+    stdest=n.sqrt(stdest/n_avg)
+    if plot:
+        plt.imshow(stdest,vmin=0,vmax=10.0)
         plt.colorbar()
         plt.show()
+    return(stdest)
 
+def diff_img_stack(fname="full_59.mp4",
+                   cam_0=1490,
+                   cam_1=1536,
+                   max_skip=2,        # maximum frame number difference
+                   max_vel=40,        # at least one pixel and at most 50 pixel per frame movement.
+                   min_vel=0.1,       # this many pixels per frame
+                   min_n_frames=4,
+                   dec=4,
+                   history_len=1000,
+                   debug_print=False,
+                   n_frames=15):
     
+    stdest=img_var_est(fname=fname,
+                       cam_0=cam_0,
+                       cam_1=cam_1,
+                       dec=dec,
+                       plot=False,
+                       median_len=30)
+
+    det_frames = []
+    det_pos = []
+    det_snr = []    
+
+    scale=None
+    
+    cap = cv2.VideoCapture(fname)
+    ret,frame0 = cap.read()
+    frame0[cam_0:cam_1,:,:]=0
+    history=n.zeros(history_len)
+
+    gray = cv2.cvtColor(frame0,cv2.COLOR_BGR2GRAY)
+    prev=n.array(block_reduce(gray,block_size=(dec,dec),func=n.max),dtype=n.float32)
+    frame_num=1
+
+    prev_det_frame=0
+    prev_det_xi=-1
+    prev_det_yi=-1
+
+    det_idx=0
+
+    while(1):
+        ret,frame0 = cap.read()
+        if ret:
+            frame0[cam_0:cam_1,:,:]=0
+            gray = cv2.cvtColor(frame0,cv2.COLOR_BGR2GRAY)
+            gray2=n.array(block_reduce(gray,block_size=(dec,dec),func=n.max),dtype=n.float32)
+            diff_im = gray2-prev
+
+            scaled_diff=diff_im/stdest/n.sqrt(2.0)
+
+            
+            max_diff=n.max(scaled_diff)
+
+            # difference image maximum at least 3 stdev
+            if max_diff > 8.0:
+                frame_r=block_reduce(n.array(frame0,dtype=n.float32),block_size=(dec,dec,1),func=n.mean)
+
+                if scale == None:
+                    fr2=n.copy(frame_r)
+                    fs=n.sort(fr2.flatten())
+                    max_img_val=fs[int(0.95*len(fs))]
+                    scale=n.min([255.0/max_img_val,8.0])
+                frame_r=frame_r*scale
+                frame_r[frame_r > 255.0]=255.0
+                frame_r=n.array(frame_r,dtype=n.uint8)
+                
+                ofname="%s-%04d.jpg"%(fname,frame_num)
+                print("saving %s"%(ofname))
+                cv2.imwrite(ofname, frame_r)
+
+                # what is the x and y index of this
+                idx=n.argmax(diff_im)
+                xi,yi=n.unravel_index(idx,diff_im.shape)
+
+                # value of pixel
+                max_val=scaled_diff[xi,yi]
+
+                print("possible det %d %d,%d snr %1.2f scale %1.2f"%(frame_num,xi,yi,max_val,scale))
+                det_frames.append(frame_num)
+                det_pos.append((xi,yi))
+                det_snr.append(max_val)
+                det_idx+=1
+
+            prev=gray2
+            frame_num+=1
+
+        else:
+            break
+    cap.release()
+    return(det_frames,det_pos,det_snr)
+
+
+if __name__ == "__main__":
+#    fl=glob.glob("examples/*.mp4")    
+    fl=glob.glob("/data0/cam1/21/*.mp4")
+    fl.sort()
+
+    for fi in range(comm.rank,len(fl),comm.size):
+        f=fl[fi]
+        print(f)
+        diff_img_stack(fname=f)        
+#        det_frames,det_pos=diff_img_stack(fname=f)
+
+        
 
