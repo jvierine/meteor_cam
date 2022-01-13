640b051d0a37f0ad12bb510af17accade27e27a3
Juha Vierinen <jvierine@gmail.com>
2022-01-13 08:25:29 +0000

    added first stab at this

---
+++ install-astrometry.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash 
+sudo apt-get install astrometry.net libastrometry-dev libastrometry0
+cd /usr/share/astrometry
+sudo wget http://broiler.astrometry.net/\~dstn/4100/index-4119.fits
+sudo wget http://broiler.astrometry.net/\~dstn/4100/index-4118.fits
+sudo wget http://broiler.astrometry.net/\~dstn/4100/index-4117.fits
+sudo wget http://broiler.astrometry.net/\~dstn/4100/index-4116.fits 
